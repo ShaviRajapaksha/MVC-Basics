5161905f259bfe7eb04c9e264d3b4fa40754e1fa
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-08-13 00:24:26 +0530

    Add Member model with properties and borrowings collection

---
+++ LibraryHub/Models/Member.cs
@@ -0,0 +1,11 @@
+namespace LibraryHub.Models;
+
+public class Member
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+
+    public ICollection<Borrowing> Borrowings { get; set; }= new List<Borrowing>();
+}
